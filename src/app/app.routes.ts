1a4b81b37f60d6f1fd36e9bb7c25daadb642c274
Alex Hyland-Cid <alexander.hylandcid@gmail.com>
2023-04-10 19:05:51 +0200

    third commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,5 +8,9 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
   }
 ];
