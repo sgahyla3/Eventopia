763e3fd19fdc00930792eca9fff18b861f980bbf
brindle11 <79323821+brindle11@users.noreply.github.com>
2023-04-18 04:21:52 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,51 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
+import { LoginPage } from './pages/login/login.page';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule],
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginPage
+  },
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        loadComponent: () =>
+          import('./tab1/tab1.page').then((m) => m.Tab1Page),
+      },
+      {
+        path: 'tab2',
+        loadComponent: () =>
+          import('./tab2/tab2.page').then((m) => m.Tab2Page),
+      },
+      {
+        path: 'tab3',
+        loadComponent: () =>
+          import('./tab3/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: 'tab4',
+        loadComponent: () =>
+          import('./tab4/tab4.page').then((m) => m.Tab4Page),
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
-export class AppComponent {
-  public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {}
-}
+export class AppRoutingModule { }
