e46147d54ca671300148740b2393b0ddcaaec8e2
brindle11 <79323821+brindle11@users.noreply.github.com>
2023-04-18 13:10:46 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,63 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TabsPage } from './tabs/tabs.page';
+import { LoginPage } from './pages/login/login.page';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    component: LoginPage
   },
   {
     path: 'register',
     loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
   },
   {
+    path: 'friends',
+    loadComponent: () => import('./pages/friends/friends.page').then( m => m.FriendsPage),
+  },
+  {
     path: 'event',
     loadComponent: () => import('./pages/event/event.page').then( m => m.EventPage)
   },
   {
-    path: 'friends',
-    loadComponent: () => import('./pages/friends/friends.page').then( m => m.FriendsPage)
-  },
-  {
-    path: 'tab4',
-    loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        loadComponent: () =>
+          import('./tab1/tab1.page').then((m) => m.Tab1Page),
+      },
+      {
+        path: 'tab2',
+        loadComponent: () =>
+          import('./tab2/tab2.page').then((m) => m.Tab2Page),
+      },
+      {
+        path: 'tab3',
+        loadComponent: () =>
+          import('./tab3/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: 'tab4',
+        loadComponent: () =>
+          import('./tab4/tab4.page').then((m) => m.Tab4Page),
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+    ]
   }
-];
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
