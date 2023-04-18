d7bbdec9d7580a0c4e5dd61cbf6a5420c9957b67
brindle11 <79323821+brindle11@users.noreply.github.com>
2023-04-19 00:25:53 +0100

    Update tab4.page.ts

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
 })
 export class Tab4Page implements OnInit {
 
